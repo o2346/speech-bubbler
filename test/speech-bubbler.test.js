dcfb3551b39b0f4e376f49ddfa45e11637fb4b5a
user <magi@magi-desktop>
2019-07-20 17:58:04 +0900

    update tests

--- test/speech-bubbler.test.js
+++ test/speech-bubbler.test.js
@@ -1,9 +1,42 @@
-const assert = require( 'assert' );
-const Bubbler = require( '../speech-bubbler.js' );
-const bubbler = new Bubbler();
+const assert   = require( 'assert' );
+const fs       = require( 'fs' );
+const Bubbler  = require( '../speech-bubbler.js' );
+const bubbler  = new Bubbler();
+const contents = require( './contents.json' );
 
 describe( 'speech-bubbler', () => {
-  it( 'should return -1 when the value is not present', () => {
-    assert.equal( [1, 2, 3 ].indexOf( 4 ), -1 );
-  } );
+
+  contents.bubblerRender
+    .forEach( ( obj, i ) => {
+      const subject = String().concat(
+        'render ',
+        i,
+        ' ',
+        obj.text.slice( 0, 10 ).replace( /[\n\r]/g, '' ),
+        '...'
+      );
+
+      it( subject, () => {
+        const expected = fs.readFileSync( 'test/' + obj.path ).toString();
+        const actual   = bubbler.render( obj.text, obj.option );
+        assert.deepEqual( expected, actual );
+      } );
+    } );
+
+  contents.vertmap
+    .forEach( ( obj, i ) => {
+      const subject = String().concat(
+        'vertmap ',
+        i,
+        ' ',
+        obj.text.slice( 0, 10 ).replace( /[\n\r]/g, '' ),
+        '...'
+      );
+
+      it( subject, () => {
+        const expected = fs.readFileSync( 'test/' + obj.path ).toString();
+        const actual   = bubbler.vertmap( obj.text );
+        assert.deepEqual( expected, actual );
+      } );
+    } );
 } );
